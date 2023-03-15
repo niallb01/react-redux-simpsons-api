c80f8aac4a25ac497dc1cdb177329bbdd49ffc08
niallb01 <89789937+niallb01@users.noreply.github.com>
2023-03-15 18:53:17 +0000

    Add files via upload

---
+++ src/app/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import characterReducer from "../features/counter/characterSlice";
+//store makes data available to components
+
+export const store = configureStore({
+  reducer: {
+    character: characterReducer,
+  },
+});
